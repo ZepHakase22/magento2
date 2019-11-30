15aa57e8a2b3c7e06e760c3e9b7020e4eb95b0c1
Diego Marino Monetti <ZepHakase22@gmail.com>
2019-11-30 18:26:24 +0000

    New Start

--- pub/static/adminhtml/Magento/backend/en_US/Magento_ConfigurableProduct/js/components/modal-configurable.js
+++
@@ -1,1 +0,0 @@
-/var/www/html/magento2/app/code/Magento/ConfigurableProduct/view/adminhtml/web/js/components/modal-configurable.js
--- pub/static/adminhtml/Magento/backend/en_US/Magento_Rule/rules.js
+++
@@ -1,1 +0,0 @@
-/var/www/html/magento2/app/code/Magento/Rule/view/adminhtml/web/rules.js
